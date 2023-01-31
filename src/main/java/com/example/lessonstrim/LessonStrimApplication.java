ccad519dcb86e8c88bcb3b526ec586d272452e71
Людок <skobeleva.ludmila@yandex.ru>
2023-02-01 00:09:43 +0300

    Initial commit

---
+++ src/main/java/com/example/lessonstrim/LessonStrimApplication.java
@@ -0,0 +1,13 @@
+package com.example.lessonstrim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonStrimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LessonStrimApplication.class, args);
+	}
+
+}
